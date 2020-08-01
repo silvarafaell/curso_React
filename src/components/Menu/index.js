ac8af44f69c89a48fd7d78bf22d6e46a07e08b80
francisco rafael <francisco_rafael@hotmail.com.br>
2020-08-01 08:24:56 -0300

    final segunda aula - Roteamento e state

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import FutebolFlix from '../../assets/img/FutebolFlix.png';
 import './Menu.css'
 import Button from '../Button'
@@ -8,11 +9,11 @@ function Menu() {
     return (
       
         <nav className="Menu">
-            <a href="/">
+            <Link to="/">
            <img className="FutebolFlix" src={FutebolFlix} alt="FutebolFlix FutebolFlix" />
-            </a>
+            </Link>
 
-          <Button as='a' className="ButtonLink" href="/">
+          <Button as={Link} className="ButtonLink" to="/cadastro/video">
             Novo vídeo
           </Button>
         </nav>   
